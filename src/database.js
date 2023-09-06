34834bec5a44c433af904e2a7a045027237c81df
camilo <cetacue@misena.edu.co>
2023-09-06 17:46:17 -0500

    2

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-const URI='mongodb://localhost/mern-task'
+const URI='mongodb://127.0.0.1/mern-task';
 mongoose.connect(URI)
 .then(db=>console.log('db esta conectada'))
 .catch(err=>console.log(err));
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const router=express.Router();
+const Task=require('../models/task');
 router.get('/',(req,res)=>{
+    Task.find().exec((err, tasks) => {
+        console.log(tasks);
+    })
     res.json({
         status:'si funciona'
-    })
+    });
 })
 module.exports=router;
