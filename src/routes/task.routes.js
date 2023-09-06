6fa4d8bd2067b8480b1478e9baf893267a04f209
camilo <cetacue@misena.edu.co>
2023-09-06 18:11:12 -0500

    camilo

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,9 +2,9 @@ const express=require('express');
 const router=express.Router();
 const Task=require('../models/task');
 router.get('/',(req,res)=>{
-    Task.find().exec((err, tasks) => {
-        console.log(tasks);
-    })
+    Task.find({})
+        .then(tasks => console.log(tasks))
+        .catch(err => console.error(err));
     res.json({
         status:'si funciona'
     });
