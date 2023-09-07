e6db7460367a54a4d684b5055494de760ffd1e4d
camilo <cetacue@misena.edu.co>
2023-09-07 14:16:23 -0500

    rutas se servidor terminadas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,34 @@
 const express=require('express');
 const router=express.Router();
 const Task=require('../models/task');
-router.get('/',(req,res)=>{
-    Task.find({})
-        .then(tasks => console.log(tasks))
-        .catch(err => console.error(err));
-    res.json({
-        status:'si funciona'
-    });
+const task = require('../models/task');
+router.get('/',async(req,res)=>{
+ const tasks=await Task.find();
+ res.json(tasks);
 })
+router.get('/:id',async(req,res)=>{
+  const task = await Task.findById(req.params.id)
+  res.json(task);
+})
+//---------------------
+router.post('/',async(req,res)=>{
+   const {title,description}=req.body;
+   const task=new Task({title,description})
+   await task.save()
+   console.log(task)
+   res.json({status:'tarea guardada'})
+   })
+
+   router.put('/:id',async(req,res)=>{
+    const {title,description}=req.body;
+    const newTask={title,description}
+    await Task.findByIdAndUpdate(req.params.id,newTask);
+    res.json({status:'tarea actualizada'})
+   
+   })
+
+   router.delete('/:id',async(req,res)=>{
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status:'tarea eliminada'});
+   })
 module.exports=router;
